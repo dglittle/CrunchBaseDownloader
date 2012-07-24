4d6f27f0494ce1d9508055f5b87fbf8a287f400b
Greg Little <glittle@gmail.com>
2012-07-24 16:33:24 -0700

    blah

---
+++ copyToS3.js
@@ -0,0 +1,22 @@
+
+firstRun = typeof(firstRun) == "undefined" 
+if (firstRun)
+    require('fibers')
+
+Fiber(function () {
+    fs = require('fs')
+    url = require('url')
+    querystring = require('querystring')
+    exec = require('child_process').exec
+    crypto = require('crypto')
+    http = require('http')
+
+    require('./myutil')
+    require('./nodeutil')
+    
+    s3 = require('s3')(process.argv[2], process.argv[3], process.argv[4])
+    
+    s3.put('crunchbase.zip', fs.readFileSync('./crunchbase.zip'), true)
+    
+}).run()
+
