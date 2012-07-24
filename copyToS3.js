e2459a652e19b254b1365ff1cb86826b76f892fe
Greg Little <glittle@gmail.com>
2012-07-24 16:37:51 -0700

    blah

--- copyToS3.js
+++ copyToS3.js
@@ -14,7 +14,7 @@ Fiber(function () {
     require('./myutil')
     require('./nodeutil')
     
-    s3 = require('./s3')(process.argv[2], process.argv[3], process.argv[4])
+    s3 = new (require('./s3').s3)(process.argv[2], process.argv[3], process.argv[4])
     
     s3.put('crunchbase.zip', fs.readFileSync('./crunchbase.zip'), true)
     
